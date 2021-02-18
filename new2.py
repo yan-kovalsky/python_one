4888f040ba141282cd0e8e3c2c0b7fdb05cf9692
Ivan Kamaev <kamaev@palex.ru>
2021-02-18 15:50:50 +0700

    findMatch

--- new2.py
+++ new2.py
@@ -1,11 +1,17 @@
 import os
 from glob import glob
-
-#получаем имя файлов в указанной директории
+import shutil
+#получаем имя всех файлов в указанной директории - эти файлы надо будет записать поверх старых
 path = 'C:/Users/kamaev/Desktop/OTHER/Python/testing/New folder'
 for root,dirs,files in os.walk(path):
-     targetFilesArray = (files)
-     # print(targetFilesArray)
+     targetFilesArray = files
+     # array1 =[]
+     # for i in targetFilesArray:
+     #     array2 = array1.append(os.path.join(root,i))
+
+
+     print(targetFilesArray)
+     # print (array1)
 
 # функция для поиска конкретного файла в целевой директории и поддиректториях,
 # возвращает список, содержащий путь и имя файла
@@ -15,14 +21,33 @@ def findFiles (fileName,path):
         files.extend(glob(os.path.join(dir, fileName)))
     return files
 
-#для каждого файла из указанной выполняем фукнцию поиска
+#для каждого файла выполняем фукнцию поиска
 for x in targetFilesArray:
     path = 'C:/Users/kamaev/Desktop/OTHER/Python/testing/Old folder tree'
     a = findFiles (x,path)
-# перебор элементов списка
+    # print(a)
+
+# делаем из списков сорса и таргета экземпляры файлов
     i = 0
     while i < len(a):
         targetFile = (a[i])
         print (targetFile)
         i += 1
-    # print (a)
+
+    # i = 0
+    # while i < len(array1):
+    #     sourceFile = (array1[i])
+    #     print (sourceFile)
+    #     i += 1
+
+
+# перезаписываем дубликаты
+# pathToFile = 'C:/Users/kamaev/Desktop/OTHER/Python/testing/New folder/'
+# for root,dir,files in os.walk(pathToFile):
+#     files1=files
+#     for file in files1:
+#         sourceArray = []
+#         for i in file:
+            array2 = sourceArray.append(os.path.join(root,i))
+            rewright = shutil.copyfile(array2,targetFile)
+            print (rewright)
