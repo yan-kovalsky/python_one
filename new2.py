1e817148e1963db085e1d66af25580d807ac913f
Ivan Kamaev <kamaev@palex.ru>
2021-02-18 16:40:30 +0700

    сделали перезапись

--- new2.py
+++ new2.py
@@ -5,13 +5,13 @@ import shutil
 path = 'C:/Users/kamaev/Desktop/OTHER/Python/testing/New folder'
 for root,dirs,files in os.walk(path):
      targetFilesArray = files
-     # array1 =[]
-     # for i in targetFilesArray:
-     #     array2 = array1.append(os.path.join(root,i))
+     array1 =[]
+     for i in targetFilesArray:
+         array1.append(os.path.join(root,i))
 
 
-     print(targetFilesArray)
-     # print (array1)
+     # print(targetFilesArray)
+print (array1)
 
 # функция для поиска конкретного файла в целевой директории и поддиректториях,
 # возвращает список, содержащий путь и имя файла
@@ -19,35 +19,33 @@ def findFiles (fileName,path):
     files =[]
     for dir,_,_ in os.walk(path):
         files.extend(glob(os.path.join(dir, fileName)))
+
     return files
+
 
 #для каждого файла выполняем фукнцию поиска
 for x in targetFilesArray:
     path = 'C:/Users/kamaev/Desktop/OTHER/Python/testing/Old folder tree'
     a = findFiles (x,path)
-    # print(a)
+    if len(a) > 1:
+        print('Для файла', x, 'в целевой директории найдены несколько совпадений!:',a)
+    else:
+       final = a
+       print(final)
 
-# делаем из списков сорса и таргета экземпляры файлов
+# делаем из списков экземпляры файлов
     i = 0
-    while i < len(a):
-        targetFile = (a[i])
-        print (targetFile)
+    while i < len(array1):
+        sourceFile = (array1[i])
+        # print (sourceFile)
         i += 1
 
-    # i = 0
-    # while i < len(array1):
-    #     sourceFile = (array1[i])
-    #     print (sourceFile)
-    #     i += 1
+    i = 0
+    while i < len(final):
+        targetFile = (final[i])
+        # print (targetFile)
+        i += 1
 
-
-# перезаписываем дубликаты
-# pathToFile = 'C:/Users/kamaev/Desktop/OTHER/Python/testing/New folder/'
-# for root,dir,files in os.walk(pathToFile):
-#     files1=files
-#     for file in files1:
-#         sourceArray = []
-#         for i in file:
-            array2 = sourceArray.append(os.path.join(root,i))
-            rewright = shutil.copyfile(array2,targetFile)
-            print (rewright)
+ # перезаписываем дубликаты
+rewright = shutil.copyfile(sourceFile,targetFile)
+print (rewright)
